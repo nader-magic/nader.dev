66ac879ad60d75bfbf64839977b26f7063725e70
nader.dev <118058589+nader-magic@users.noreply.github.com>
2024-11-22 15:32:23 +0200

    Create Process.js

---
+++ Process.js
@@ -0,0 +1,14 @@
+function calc(){
+  var x = document.getElementById('n1').value;
+
+  var y = document.getElementById('n2').value;
+
+  var z = eval(x) + eval(y);
+
+  document.getElementById('s').value = z;
+
+}
+
+body = document
+
+tag  = element
